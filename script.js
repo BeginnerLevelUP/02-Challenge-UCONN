34cb5b89b4934a5b81d3f1ddfe7833ac1afc0f9d
Damion Morgan <only4school5680@gmail.com>
2023-08-02 22:16:35 -0400

    ui is done just need add handlebars code and need to create a button to esc for mobile users and all images have to have alts

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 const fullstack=document.getElementsByClassName('FULLSTACK')[0]
 const projects=document.getElementsByClassName('projects')
 const work=document.getElementsByClassName('work')[0];
-const workAside=work.children[0]
+
+  
+/*  DECIDED TO USE BOOTSTRAP CAROUSEL
+
+
 // Function to get a full view of the project
 const fullView=()=>{
     for(let i=0;i<projects.length;i++){
@@ -44,4 +48,42 @@ window.addEventListener('keydown',function(e){
           fullstack.style.height = ''; 
           fullstack.style.margin = ''; 
     }
+})*/
+// Full View
+const CAROUSEL=()=>{
+  work.children[0].style.display='none'
+  work.children[1].style.display='none'
+  work.children[2].style.display='block' 
+}
+fullstack.addEventListener('click',CAROUSEL)
+fullstack.addEventListener('tap',CAROUSEL)
+
+for(let i=0;i<projects.length;i++){
+  projects[i].addEventListener('click', CAROUSEL);
+  projects[i].addEventListener('tap', CAROUSEL);
+}
+
+window.addEventListener('keydown',function(event){
+  if(event.key==='Escape'){
+for(let i=0;i<work.children.length;i++){
+      work.children[i].style.display='';
+    }
+  }
 })
+//Change innerHtml of phone number and email when you click
+
+
+  const phoneNum=document.getElementById('phoneNum');
+  const email=document.getElementById('email')
+
+  phoneNum.addEventListener('click',function(){
+    phoneNum.innerHTML='718-724-9000';
+  })
+  
+  email.addEventListener('click',function(){
+    email.innerHTML='morgandamion5680@gmail.com'
+  })
+
+  
+// Handle Bars
+// mkae sure all images has fonts
